f2c7a799bb7eb15762991e22ad32b83e8515c260
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2025-02-14 17:00:25 +0530

    Socket.io Added

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {io} from "socket.io-client";
+import {useEffect } from "react";
+
+const App = () => {
+
+  const socket = io("http://localhost:3000/");
+
+  useEffect(() =>{
+    socket.on("connect", () =>{
+      console.log("Connected");
+    })
+
+    socket.on("welcome", (s) =>{console.log(s)});
+
+    return () =>{
+      socket.disconnect();
+    }
+  }, []);
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const PORT = 3000;
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173", // Removed trailing slash
+        methods: ['GET', 'POST'],
+        credentials: true,
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+io.on("connection",(socket) =>{
+    console.log("User connected");
+    console.log("ID", socket.id);
+
+    // socket.emit("welcome", "Welcome to the server");
+
+    // socket.broadcast.emit("welcome", `${socket.id} joined the server`);
+
+    socket.on("message", (data) =>{
+        console.log(data);
+    })
+
+    socket.on("disconnect", () =>{
+        console.log("User disconnected", socket.id);
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
